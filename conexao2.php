f8ab47e101f0be35391545b21e2e0cc4cc7ff068
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 18:28:28 -0300

    Update conexao2.php

--- conexao2.php
+++ conexao2.php
@@ -1,15 +1,13 @@
 <?php
 
-    $dbname="techladies";
-    $dbuser="admin";
-    $dbpassword="jaPs4c50vBfg";
+$servidor = "localhost";
+$usuario = "admin";
+$senha="jaPs4c50vBfg";
+$dbname="techladies";
 
-    try{
-        $conn=new PDO('mysql:host=localhost:8080;dbname='.$dbname,$dbuser,$dbpassword); #1
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); #2
-    }catch(PDOException $error){
-        return '<h3>Erro de conexão:</h3><p>'.$error->getMessage().'</p>';
-    }
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+
 
 
 ?>
