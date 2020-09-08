6c440f8197e8c8071fe948e095deb3ee70173421
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 17:55:14 -0300

    Update gravar2.php

--- gravar2.php
+++ gravar2.php
@@ -7,9 +7,10 @@ $sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
-
-//echo "Nome: $nome <br>";
-//echo "E-mail: $email <br>";
+echo "Nome: $nome <br>";
+echo "Sobrenoe: $sobrenome <br>";
+echo "E-mail: $email <br>";
+echo "Senha: $senha <br>";
 
 $result_usuario = "INSERT INTO usuarios (nome, sobrenome, email, senha, created) VALUES ('$nome', '$sobrenome', '$email', '$senha', NOW())";
 
