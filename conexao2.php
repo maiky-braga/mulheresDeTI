35f9ea4b5107b50d26b3fb580a75065122ea22df
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 18:20:38 -0300

    Update conexao2.php

--- conexao2.php
+++ conexao2.php
@@ -1,10 +1,13 @@
 <?php
-$servidor = "127.0.0.1:3306";
+$servidor = "127.0.0.1:8080";
 $usuario = "admin";
 $senha="jaPs4c50vBfg";
 $dbname="techladies";
 
 //Criar a conexao
 $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
-
+if (!$conn) {
+        die('Erro na conexão: '.mysql_error()); #2
+    }
+    mysql_close(); #3
 ?>
