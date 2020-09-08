fb1945057d7bb94f0adc1d63006cd3617d342e9b
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 17:14:58 -0300

    Add files via upload

--- gravar2.php
+++ gravar2.php
@@ -1,9 +1,24 @@
 <?php
 
-$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
-$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+include_once("conexao.php");
 
-echo "Nome: $nome <br>";
-echo "E-mail: $email <br>";
+$nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
+$sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);=
+
+
+//echo "Nome: $nome <br>";
+//echo "E-mail: $email <br>";
+
+$result_usuario = "INSERT INTO usuarios (nome, sobrenome, email, senha, created) VALUES ('$nome', '$sobrenome', '$email', '$senha', NOW())";
+
+$resultado_usuario = mysqli_query($conn, $result_usuario);
+
+if (mysqli_insert_id($conn)){
+	header("Location: index.html");
+}else{
+	header("Location: index.html");
+}
 
 ?>
