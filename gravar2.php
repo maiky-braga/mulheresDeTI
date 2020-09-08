ef1d468aed4d21eabe4dbd18da1eed1bbb00ba53
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 17:45:53 -0300

    Update gravar2.php

--- gravar2.php
+++ gravar2.php
@@ -5,7 +5,7 @@ include_once("conexao.php");
 $nome = filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_STRING);
 $sobrenome = filter_input(INPUT_POST, 'sobrenome', FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);=
+$senha = filter_input(INPUT_POST, 'senha', FILTER_SANITIZE_STRING);
 
 
 //echo "Nome: $nome <br>";
