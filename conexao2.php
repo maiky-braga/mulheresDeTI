d8309d68b1c36e2ea6cb6db861d78a67eeda7f0c
ykiam <49007140+ykiam-dyolf@users.noreply.github.com>
2020-09-08 18:43:47 -0300

    Update conexao2.php

--- conexao2.php
+++ conexao2.php
@@ -1,14 +1,20 @@
 <?php
 
-$servidor = "127.0.0.1:8080";
+$servidor = "127.0.0.1";
 $usuario = "admin";
 $senha="jaPs4c50vBfg";
 $dbname="techladies";
+$port="8080";
 
 //Criar a conexao
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname,$port);
 
 
+if (mysqli_connect_errno()) {
+   echo "Falha na conexão com o servido MySQL:  " . mysqli_connect_error();
+} else {
+   echo 'Conexão Ok!';
+}
 
 ?>
 
