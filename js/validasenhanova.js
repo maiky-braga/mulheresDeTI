3e735c31fff7036238482e74e4ba86d8998ed719
adudars <eduardarsimoes@gmail.com>
2020-09-24 12:08:27 -0300

    perfil off

--- js/validasenhanova.js
+++
@@ -1,9 +0,0 @@
-function validar() {
-    var novasenhaverifica = form_senha.novasenhaverifica.value;
-    var novasenha = form_senha.novasenha.value;
-
-    if (novasenha != novasenhaverifica){
-        alert('Senhas não conferem!');
-        form_senha.novasenhaverifica.focus();
-    }
-}
